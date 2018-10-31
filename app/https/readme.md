4dc33d6995ffea447308012487f7c55cd45f4bc6
Quan Nguyen <minhquanit@gmail.com>
2018-10-31 14:53:57 -0500

    https

---
+++ app/https/readme.md
@@ -0,0 +1,2 @@
+Run this command to generate key
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -keyout key.pem -out cert.pem
--- app/index.js
+++ app/index.js
@@ -1,12 +1,25 @@
 // Dependencies
 const http = require('http');
+const https = require('https');
 const url = require('url');
 const StringDecoder = require('string_decoder').StringDecoder;
 const config = require('./config');
+const fs = require('fs');
 
-// The server should respond to all requests with a string
-const server = http.createServer((req, res) => {
+// Instantiate and start the HTTP server
+const httpServer = http.createServer((req, res) => unifiedServer(req, res));
+httpServer.listen(config.httpPort, () => console.log(`The server is listening on port ${config.httpPort} in ${config.envName}`));
 
+// Instantiate and start the HTTPS server
+const httpsServerOptions = {
+  'key': fs.readFileSync('./https/key.pem'),
+  'cert': fs.readFileSync('./https/cert.pem')
+};
+const httpsServer = https.createServer(httpsServerOptions, (req, res) => unifiedServer(req, res));
+httpsServer.listen(config.httpsPort, () => console.log(`The server is listening on port ${config.httpsPort} in ${config.envName}`));
+
+// All the server logic for both http and https server
+const unifiedServer = (req, res) => {
   // Get the url and parse it
   var parsedUrl = url.parse(req.url, true);
 
@@ -62,10 +75,7 @@ const server = http.createServer((req, res) => {
       console.log('Returning this response: ', statusCode, payloadString);
     });
   });
-});
-
-// Start the server
-server.listen(config.port, () => console.log(`The server is listening on port ${config.port} in ${config.envName}`));
+};
 
 // Define the handlers
 var handlers = {};
