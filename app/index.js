afd4a499937f31a1e9c0a52cc48b68438be84ee2
Quan Nguyen <minhquanit@gmail.com>
2018-10-31 12:47:04 -0500

    Parsing payloads

--- app/index.js
+++ app/index.js
@@ -1,16 +1,17 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
-  
+
   // Get the url and parse it
   var parsedUrl = url.parse(req.url, true);
 
   // Get the path
   var path = parsedUrl.pathname;
-  var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
   // Get the query string as an object
   var queryStringObject = parsedUrl.query;
@@ -21,12 +22,21 @@ const server = http.createServer((req, res) => {
   // Get the headers as an object
   var headers = req.headers;
 
-  // Send the response
-  res.end('Hello World\n');
+  // Get the payload, if any
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
+  req.on('data', (data) => buffer += decoder.write(data));
+  req.on('end', () => {
+    buffer += decoder.end();
 
-  // Doing some loggings
-  console.log(`Request received on path: ${trimmedPath} with method: ${method} and with these query string parameters:`, queryStringObject);
-  console.log('Request received with these headers: ', headers);
+    // Send the response
+    res.end('Hello World\n');
+
+    // Doing some loggings
+    console.log(`Request received on path: ${trimmedPath} with method: ${method} and with these query string parameters:`, queryStringObject);
+    console.log('Request received with these headers: ', headers);
+    console.log('Request received with this payload: ', buffer);
+  });
 });
 
 // Start the server, and have it listen on port 3000
